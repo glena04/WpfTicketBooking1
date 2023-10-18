ddee260ccc4920179a85e8e06483d14265bc10ea
star <starbahadin@hotmail.com>
2023-10-18 23:05:17 +0200

    Add project files.

---
+++ Bookings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTicketBooking1
+{
+    public class Bookings
+    {
+        public string Evenemangsnamn { get; set; }
+        public string Datum { get; set; }
+        public string Tid { get; set; }
+        public string Users { get; set; }
+    }
+
+}
